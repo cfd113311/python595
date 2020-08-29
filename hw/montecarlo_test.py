9eb9445b2cc5ac8828b33061c661f74129326afb
Jarret <iamjarret@gmail.com>
2020-08-29 16:44:29 -0400

    hw change

--- hw/montecarlo_test.py
+++ hw/montecarlo_test.py
@@ -9,25 +9,16 @@
 # 3. Integral value is average function value times
 #   domain length.
 
-
-from montecarlo import Engine, 2DNumericalIntegration
+from montecarlo import 2DNumericalIntegrator
 
 import math
 
-# implementing these objects is the assessment for the Python module.
-
 if __name__ == "__main__":
-    engine = Engine() # default value draws from a uniform distribution
+    numerical_integrator = 2DNumercalIntegrator()
 
-    # integrator takes an engine object.
-    # engine has one method - draw(lower_bound, upper_bound)
-    # it returns a random number between the two.
-    # use the random module for this class.
-    numerical_integrator = 2DNumercalIntegration( engine )
+   numerical_integrator.set_objective( math.sin) # set objective function to evaluate.
 
-   numerical_integrator.set_objective( math.sin) # set objective function as sin
-
-    numerical_integrator.set_scenarios(1000) # use this many iterations
+    numerical_integrator.set_scenarios(1000) # use this many draws.
 
     lower_bound = 0.0
     upper_bound = 2 * math.pi
